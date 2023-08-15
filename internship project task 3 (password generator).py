94f9275c60dd5532a32bb945bffbd8c2d5454504
Vivek1925sharma <140584943+Vivek1925sharma@users.noreply.github.com>
2023-08-15 16:41:48 -0400

    Add files via upload

---
+++ internship project task 3 (password generator).py
@@ -0,0 +1,63 @@
+from tkinter import *
+import string
+import random
+
+
+def generator():
+    small_alphabets=string.ascii_lowercase
+    capital_alphabets=string.ascii_uppercase
+    numbers=string.digits
+    special_characters=string.punctuation
+
+    all=small_alphabets+capital_alphabets+numbers+special_characters
+    password_length=int(length_Box.get())
+
+    if choice.get()==1:
+        passwordField.insert(0,random.sample(small_alphabets,password_length))
+
+    if choice.get()==2:
+        passwordField.insert(0,random.sample(small_alphabets+capital_alphabets,password_length))
+
+    if choice.get()==3:
+        passwordField.insert(0,random.sample(all,password_length))        
+
+    password=random.simple(all,password_length)
+    passwordField.insert(0,password)
+
+def copy():
+    random_password=passwordField.get()
+    pyperclip.copy(random_password)
+
+
+root=Tk()
+root.config(bg='gray20')
+choice=IntVar()
+font=('arial',15,'bold')
+passwordLabel=Label(root,text='Password Generator',font=('times new roman',20,'bold'),bg='gray20',fg='white')
+passwordLabel.grid(pady=10)
+
+weakradioButton=Radiobutton(root,text='Weak',value=1,variable=choice,font=font)
+weakradioButton.grid(pady=5)
+
+mediumradioButton=Radiobutton(root,text='Medium',value=2,variable=choice,font=font)
+mediumradioButton.grid(pady=5)
+
+strongradioButton=Radiobutton(root,text='Strong',value=3,variable=choice,font=font)
+strongradioButton.grid(pady=5)
+
+lengthLabel=Label(root,text='Password Length',font=font,bg='gray20',fg='white')
+lengthLabel.grid(pady=5)
+
+length_Box=Spinbox(root,from_=5,to_=18,width=5,font=font)
+length_Box.grid(pady=5)
+
+generateButton=Button(root,text='Generate',font=font,command=generator)
+generateButton.grid(pady=5)
+
+passwordField=Entry(root,width=25,bd=2)
+passwordField.grid()
+
+copyButton=Button(root,text='Copy',font=font,command=copy)
+copyButton.grid(pady=5)
+
+root.mainloop()
